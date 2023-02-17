59fb008ea3f85c2eeac91a8c22cb85df1ae96733
@ElopingDev <elopingbuisness@gmail.com>
2023-02-17 19:19:03 +0100

    first commit

---
+++ script.js
@@ -0,0 +1,10 @@
+var icon = document.getElementById("discord");
+
+var popup = document.getElementById("popup");
+
+// Add a click event listener to the icon
+icon.addEventListener("click", function() {
+    window.alert("Eloping#0001");
+
+});
+
